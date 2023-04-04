abb88ebc7094bb63212b61841de2dacdb8203be3
herve <herveperel@gmail.com>
2023-04-04 18:54:36 +0200

    vache

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '142d75b33bf3178a13228ed606c8b3080f1220e8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '142d75b33bf3178a13228ed606c8b3080f1220e8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'gipetto/cowsay' => array(
+            'pretty_version' => '1.1.0',
+            'version' => '1.1.0.0',
+            'reference' => '85218ee4c0d18cc8753e08af54a8740aac12860a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../gipetto/cowsay',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
